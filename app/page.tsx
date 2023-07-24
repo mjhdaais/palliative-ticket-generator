8787cb4442445dce74f92e4e361012b4810f88b8
mjhdaais <mjhdaais@gmail.com>
2023-07-24 01:35:56 +0100

    added structure

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import styles from './styles.module.css'
 import { Roboto } from 'next/font/google'
 
 const roboto = Roboto({
-    weight: '500',
+    weight: '100',
     subsets: ['latin'],
     display: 'swap'
 })
@@ -13,14 +13,90 @@ const roboto = Roboto({
 {/* <i><FontAwesomeIcon icon={faSearch} /></i> */}
 
 export default function Page() {
-    return <header className={`${styles.container} ${styles.header}`}>
-                <div className={`${styles.row}`}>
-                    <div className={`${styles.col} ${styles.s4}`}>Column 1 content</div>
-                    <div className={`${styles.col} ${styles.s4}`}>Column 2 content</div>
-                    <div className={`${styles.col} ${styles.s4}`}>Column 3 content</div>
-                    <h1>Hello there!</h1>
+    return <header className={`${roboto.className} ${styles.header}`}>
+        {/* Navs */}
+        <div className={`${styles.container} ${styles.hide_small} ${styles.navs}`}>
+            <a href="#" className={`${styles.col} ${styles.m4}`}>About</a>
+            <a href="#" className={`${styles.col} ${styles.m4}`}>Contact</a>
+            <a href="#" className={`${styles.col} ${styles.m4}`}>FAQs</a>
+        </div>
+
+        {/* Banner */}
+        <div className={`${styles.banner}`}>
+            <div className={`${styles.container} ${styles.banner}`}>
+                <h1>Student Loan Bio-data Submission</h1>
+            </div>
+        </div>
+
+        {/* Search Field */}
+        <div className={`${styles.search_field}`}>
+            <input type="search" />
+            <button>Search</button>
+        </div>
+
+        {/* Intro */}
+        <div className={`${styles.intro}`}>
+            <div className={`${styles.container} ${styles.intro_row}`}>
+                <div className={`${styles.col} ${styles.m6}`}>
+                    <h2>Bio-data Submission</h2>
+                    <p>
+                        Bio-data evaluation plays a crucial role in assessing the financial 
+                        capacity and eligibility of applicants in governmental student loan applications. 
+                        To ensure equitable financial assessments,
+                    </p>
                 </div>
-                <h1>Hello there!</h1>
+
+                <div className={`${styles.col} ${styles.m6}`}>
+                    <h2>Student Count and Analytics</h2>
+                    <p>
+                        Analytics plays a crucial role in governmental student loan applications, 
+                        providing accurate student count information and valuable insights into student trends.
+                    </p>
+                </div>
+            </div>
+
+            <div className={`${styles.container} ${styles.intro_row}`}>
+                <div className={`${styles.col} ${styles.m6}`}>
+                    <h2>Payment of a Refundable Fee</h2>
+                    <p>
+                        As part of the official and institutional procedures, it is a mandatory requirement for all 
+                        students to remit a refundable fee payment of N200 upon the submission of their comprehensive and 
+                        accurate bio-data.
+                    </p>
+                </div>
+
+                <div className={`${styles.col} ${styles.m6}`}>
+                    <h2>Privacy and Data Protection</h2>
+                    <p>
+                        Safeguarding data privacy is paramount in governmental student loan applications to protect individuals' 
+                        personal information and ensure their trust in the process.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        {/* Sub Section */}
+        <div className={`${styles.sub_section}`}>
+            <div className={`${styles.container} ${styles.row}`}>
+                <p className={`${styles.col} ${styles.m8}`}>
+                    Promoting Equitable Financial Assessments: Empowering Students through Transparent Evaluations
+                </p>
+                <button className={`${styles.col} ${styles.m4} `}>Submit Your Bio-data Now</button>
+            </div>
+        </div>
+
+        {/* Footer */}
+        <div className={`${styles.footer}`}>
+            <div className={`${styles.container}`}>
+                <p className={`${styles.col} ${styles.m3}`}>EGL</p>
+                <p className={`${styles.col} ${styles.m3}`}>About</p>
+                <p className={`${styles.col} ${styles.m3}`}>Contact</p>
+                <p className={`${styles.col} ${styles.m3}`}>edugovloans@gmail.com</p>
+                <p className={``}>© 2019 Edugovloans. All rights reserved. </p>
+            </div>
+        </div>
+
+
                 
             {/* <section className={`${styles.header_col_i} w3-row`}>
                 <div className={`${roboto.className} ${styles.title} w3-col m12 l10`}>
