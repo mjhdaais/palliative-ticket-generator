28fa0e56c34cc84dfd2c0af3aea8c105c53ce87d
mjhdaais <mjhdaais@gmail.com>
2023-08-03 05:12:21 +0100

    fixing

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import bgImage from '@public/image/young-african-female-student-with-facemask-ho
 const Header = () => {
     return (
         <header className="header">
-            <div className="radial_background"></div>
+            <div className="radial__background"></div>
 
             <nav className="nav">
                 <div className="container">
@@ -16,39 +16,29 @@ const Header = () => {
                     </div>
                 </div>
             </nav> 
-            
+
+            <div className="container">
+                <div className="header__content">
+                    <h1>Student Loan Biodata <br /> Submission</h1>
+                    <p>Fuel Your Educational Dreams: Unlock Your Potential, Embrace Learning, and Soar Towards Success!</p>
+                    <div className="header__search">
+                    <form className="search__form">
+                        <input className="search__input" type="search" placeholder="Check Assessment Status using NIN (National Identification Number)" />
+                        <button className="search__button">Check</button>
+                    </form>
+                    </div>
+                </div>
+            </div>
+
             <Image 
                 src={bgImage} 
                 alt={"Young african female student with facemask holding her textbooks campus area"}
-                className="header_image"
+                className="header__image"
                 style={{
                     objectFit: "cover",
                     height: '100%',
                 }}
             />
-            {/* <nav className="nav">
-                <div className="container">
-                    <div className="nav__content">
-                        <div className="nav__links">
-                            <a className="nav__link" href="#">Inquery</a>
-                            <a className="nav__link" href="#">FAQS</a>
-                        </div>
-                    </div>
-                </div>
-            </nav>     */}
-        
-            <div className="container">
-            <div className="header__content">
-                <h1>Student Loan Biodata <br /> Submission</h1>
-                <p>Fuel Your Educational Dreams: Unlock Your Potential, Embrace Learning, and Soar Towards Success!</p>
-                <div className="header__search">
-                <form className="search__form">
-                    <input className="search__input" type="search" placeholder="Check Assessment Status using NIN (National Identification Number)" />
-                    <button className="search__button">Check</button>
-                </form>
-                </div>
-            </div>
-            </div>
       </header>
     )
 }
