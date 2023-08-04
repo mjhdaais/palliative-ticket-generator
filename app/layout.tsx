4dc84c89f611eff0d87dc72eb6e9f679427514e2
mjhdaais <mjhdaais@gmail.com>
2023-08-05 00:26:48 +0100

    set og

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,11 @@ import '@styles/globals.css'
 
 export const metadata = {
     title: 'Biodata Evaluation for Government Student Loans',
-    description: 'Biodata evaluation for fair student loan assessments. Discover the ethical considerations and measures for equitable access to educational loan.'
+    description: 'Biodata evaluation for fair student loan assessments. Discover the ethical considerations and measures for equitable access to educational loan.',
+    openGraph: {
+        title: 'Biodata Evaluation for Government Student Loans',
+        description: 'Biodata evaluation for fair student loan assessments. Discover the ethical considerations and measures for equitable access to educational loan.',
+      }
 }
 
 const RootLayout = ({
