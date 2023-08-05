dac50554e766f4c81cb8694372e4b86a7e93d2b9
mjhdaais <mjhdaais@gmail.com>
2023-08-05 01:47:14 +0100

    fixed og img

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import '@styles/globals.css'
+import og_image from '@public/image/og/og_image.png'
 
 export const metadata = {
     title: 'Biodata Evaluation for Government Student Loans',
@@ -8,11 +9,12 @@ export const metadata = {
         description: 'Biodata evaluation for fair student loan assessments. Discover the ethical considerations and measures for equitable access to educational loan.',
         images: [
             {
-                url: '@public/image/og/og_image.png',
+                url: 'https://drive.google.com/file/d/1hZZN9UPqh8cuskhNU5vNdWRXDUDYhxE7/view?usp=drive_link',
                 width: 800,
                 height: 600,
               },
-        ]
+        ],
+        url: 'https://education-loan.vercel.app/'
     }
 }
 
