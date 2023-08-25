6197ad49adae14f52efcca7bea417ea9976fff6c
mjhdaais <mjhdaais@gmail.com>
2023-08-25 17:44:20 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Education Loan
+# Palliative Ticket Generator
--- components/GmailLabels.tsx
+++
@@ -1,7 +0,0 @@
-const GmailLabels = () => (
-  <div>
-    <h1 style={{color: 'orange'}}>System rebooting...</h1>
-  </div>
-);
-
-export default GmailLabels;
